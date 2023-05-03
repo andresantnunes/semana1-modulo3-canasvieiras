4c86e494e34c675dc1686bc27dcc9257d0d6552c
André Nunes <andre.n@edu.sc.senai.br>
2023-05-02 22:06:13 -0300

    aula1

---
+++ exemplo-aula/exemplo-aula/src/test/java/com/example/exemploaula/ExemploAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exemploaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
