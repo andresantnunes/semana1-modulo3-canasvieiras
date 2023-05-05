3b7a980ef7530d6f04ef99470233006c0ed5e5e2
André Nunes <andre.n@edu.sc.senai.br>
2023-05-04 21:53:28 -0300

    aula3

---
+++ funcionario-aula3/src/main/java/com/example/funcionarioaula3/FuncionarioAula3Application.java
@@ -0,0 +1,13 @@
+package com.example.funcionarioaula3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionarioAula3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionarioAula3Application.class, args);
+	}
+
+}
